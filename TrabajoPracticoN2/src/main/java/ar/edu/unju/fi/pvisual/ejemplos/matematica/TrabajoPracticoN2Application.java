36c69a6afaa53fcb48a1da5c87e60c4422201461
Lucas <Lucas@DESKTOP-GGLBO85>
2022-04-16 21:00:50 -0300

    proyecto

---
+++ TrabajoPracticoN2/src/main/java/ar/edu/unju/fi/pvisual/ejemplos/matematica/TrabajoPracticoN2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.ejemplos.matematica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPracticoN2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPracticoN2Application.class, args);
+	}
+
+}
